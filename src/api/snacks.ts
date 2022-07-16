1d2f78a6e4e1fa0353ebfd973a5d5caeb71842ad
Jason Francisco Macas Mora <franciscomacas3@gmail.com>
2022-07-16 12:43:43 -0500

    added Snacks to API

---
+++ src/api/snacks.ts
@@ -0,0 +1,16 @@
+const snacks: Product[] = [
+  {
+    name: "Sanduche de Pollo y tocinos",
+    ingredients: "Pan de masa madre, lechuga, tomate, queso mozzarella, pollo salteado, agridulce, tocino y aguacate.",
+    price: 5.00,
+    image: "https://images.pexels.com/photos/1647163/pexels-photo-1647163.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    category: {name: "snack"},
+  },
+  {
+    name: "Nacho sin culpa sabores: pollo, res o mixto",
+    ingredients: "Nacho, pico de gallo, poroto negro, mozzarella, salsa cheddar, guacamole.",
+    price: 5.00,
+    image: "https://images.pexels.com/photos/1647163/pexels-photo-1647163.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    category: {name: "snack"},
+  },
+]
