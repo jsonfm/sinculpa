3debb30cc7b52736ed87649e6a4c963c47a0731b
Jason Francisco Macas Mora <franciscomacas3@gmail.com>
2022-08-06 19:34:34 -0500

    Analytics key updated

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -32,7 +32,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: "G-2KCJQQCXDP",
+        trackingId: "G-FBD1WBZKHTP",
         // Defines where to place the tracking script - `true` in the head and `false` in the body
         head: true,
         // Setting this parameter is optional
