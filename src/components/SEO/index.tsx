06400216938ea7c568159de4a836f56c6eb70139
Jason Francisco Macas Mora <franciscomacas3@gmail.com>
2022-08-20 13:39:25 -0500

    SEO updated

--- src/components/SEO/index.tsx
+++ src/components/SEO/index.tsx
@@ -13,18 +13,13 @@ export const SEO = ({ title, description, pathname, children }: SEOProps) => {
     twitterUsername,
   }
 
-
   return (
     <>
       <title>{seo.title}</title>
-      <meta name="description" content={seo.description} />
-      <meta name="image" content={seo.image} />
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:title" content={seo.title} />
-      <meta name="twitter:url" content={seo.url} />
-      <meta name="twitter:description" content={seo.description} />
-      <meta name="twitter:image" content={seo.image} />
-      <meta name="twitter:creator" content={seo.twitterUsername} />
+      <meta property="og:description" content={seo.description} />
+      <meta property="og:image" content={seo.image} />
+      <meta property="og:title" content={seo.title}/>
+      <meta property="og:url" content="https://sinculpaloja.com/" />
       {children}
     </>
   )
